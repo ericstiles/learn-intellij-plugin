9cb9992f63df97d85079085db43d0078a6800fc3
GitHub Action <action@github.com>
2022-03-30 02:09:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/ericstiles/learnintellijplugin/MyBundle.kt
+++ src/main/kotlin/com/github/ericstiles/learnintellijplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.ericstiles.learnintellijplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/ericstiles/learnintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.ericstiles.learnintellijplugin.services
+
+import com.github.ericstiles.learnintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
